448fb4a6d8c544329fccf479ace3d3a7195ac8d8
Joost de Vries <joost@closealert.com>
2015-07-08 13:48:34 +0200

    Removed obsolete config

--- config/deploy.js
+++ config/deploy.js
@@ -1,37 +1,6 @@
 /* jshint node: true */
 
 module.exports = {
-  // development: {
-  //   buildEnv: 'development', // Override the environment passed to the ember asset build. Defaults to 'production'
-  //   store: {
-  //     type: 'redis', // the default store is 'redis'
-  //     host: 'localhost',
-  //     port: 6379
-  //   },
-  //   assets: {
-  //     type: 's3', // default asset-adapter is 's3'
-  //     gzip: false, // if undefined or set to true, files are gziped
-  //     gzipExtensions: ['js', 'css', 'svg'], // if undefined, js, css & svg files are gziped
-  //     exclude: ['.DS_Store', '*-test.js'], // defaults to empty array
-  //     accessKeyId: '<your-access-key-goes-here>',
-  //     secretAccessKey: process.env['AWS_ACCESS_KEY'],
-  //     bucket: '<your-bucket-name>'
-  //   }
-  // },
-
-  // staging: {
-  //   buildEnv: 'staging', // Override the environment passed to the ember asset build. Defaults to 'production'
-  //   store: {
-  //     host: 'staging-redis.example.com',
-  //     port: 6379
-  //   },
-  //   assets: {
-  //     accessKeyId: '<your-access-key-goes-here>',
-  //     secretAccessKey: process.env['AWS_ACCESS_KEY'],
-  //     bucket: '<your-bucket-name>'
-  //   }
-  // },
-
    production: {
     store: {
       type: "S3",
