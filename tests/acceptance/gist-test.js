ad720e1b6a6aece6820c59c3b858248d99438dbe
Joost de Vries <joost@closealert.com>
2015-07-11 15:38:15 +0200

    Tests should be passing now

--- tests/acceptance/gist-test.js
+++ tests/acceptance/gist-test.js
@@ -9,7 +9,7 @@ const firstFilePicker = '.code:first-of-type .dropdown-toggle';
 const secondFile = '.code:first-of-type .dropdown-menu li:nth-child(2) a';
 const anyFile = '.code:first-of-type .dropdown-menu li:nth-child(1) a';
 const fileMenu = '.file-menu .dropdown-toggle';
-const firstColumn = '.code:first-of-type';
+// const firstColumn = '.code:first-of-type';
 const deleteAction = '.file-menu a:contains(Delete)';
 
 module('Acceptance | gist', {
@@ -27,20 +27,24 @@ module('Acceptance | gist', {
 
 test('deleting a gist loaded in two columns', function(assert) {
   visit('/');
-  click(firstFilePicker);
-  click(secondFile);
-  click(firstFilePicker);
-  click(fileMenu);
-  click(deleteAction);
+
   andThen(function() {
-    assert.equal(find('.code .CodeMirror').length, 0, 'No code mirror editors active');
-    assert.equal(find('.dropdown-toggle:contains(No file selected)').length, 2, 'Shows message when no file is selected.');
-  });
-  click(firstFilePicker);
-  click(anyFile);
-  click(fileMenu);
-  click(deleteAction);
-  andThen(function() {
-    assert.equal(find('a:contains(No files available)').length, 4, 'Shows message when all files are removed.');
+    assert.equal(currentURL(), '/', 'We are on the correct route');
+    click(firstFilePicker);
+    click(secondFile);
+    click(firstFilePicker);
+    click(fileMenu);
+    click(deleteAction);
+    andThen(function() {
+      assert.equal(find('.code .CodeMirror').length, 0, 'No code mirror editors active');
+      assert.equal(find('.dropdown-toggle:contains(No file selected)').length, 2, 'Shows message when no file is selected.');
+    });
+    click(firstFilePicker);
+    click(anyFile);
+    click(fileMenu);
+    click(deleteAction);
+    andThen(function() {
+      assert.equal(find('a:contains(No files available)').length, 4, 'Shows message when all files are removed.');
+    });
   });
 });
