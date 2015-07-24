eb85db3ed1dbd7f30ebc0a67777951c37dfe766e
Gaurav Munjal <gmunjal@hedgeserv.com>
2015-07-24 13:06:58 -0400

    Fix issue cannot create two files of the same type

--- app/adapters/gist-file.js
+++ app/adapters/gist-file.js
@@ -1,7 +1,9 @@
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
+  seq: 0,
+
 	generateIdForRecord: function(store, type, inputProperties) {
-	  return inputProperties.filePath.replace(/\//gi, '.');
+	  return inputProperties.filePath.replace(/\//gi, '.') + "." + this.incrementProperty('seq');
 	}
 });
