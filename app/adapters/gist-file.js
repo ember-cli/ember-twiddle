6c0dddd86005e9fa3f3098f43149bf4695fd4ffb
Gaurav Munjal <gmunjal@hedgeserv.com>
2015-07-26 12:07:40 -0400

    Delay creating gistFile record instead of allowing duplicates

--- app/adapters/gist-file.js
+++ app/adapters/gist-file.js
@@ -1,9 +1,7 @@
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
-  seq: 0,
-
 	generateIdForRecord: function(store, type, inputProperties) {
-	  return inputProperties.filePath.replace(/\//gi, '.') + "." + this.incrementProperty('seq');
+	  return inputProperties.filePath.replace(/\//gi, '.');
 	}
 });
