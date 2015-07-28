b8e24346bed6739113955c0a513e4b937565baef
Gaurav Munjal <gmunjal@hedgeserv.com>
2015-07-27 21:54:07 -0400

    Address PR comments on unit test for confirm delete

--- tests/unit/gist/controller-test.js
+++ tests/unit/gist/controller-test.js
@@ -2,19 +2,18 @@ import Ember from "ember";
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('controller:gist', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
-  needs: ['service:ember-cli']
-});
+  needs: ['service:ember-cli'],
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  var controller = this.subject();
-  assert.ok(controller);
+  beforeEach() {
+    this._originalConfirm = window.confirm;
+  },
+
+  afterEach() {
+    window.confirm = this._originalConfirm;
+  }
 });
 
 test('deleting a gist requires confirmation', function(assert) {
-  let cacheConfirm = window.confirm;
   let controller = this.subject({
     transitionToRoute() {},
     notify: {
@@ -37,6 +36,4 @@ test('deleting a gist requires confirmation', function(assert) {
   gist = gistClass.create();
   controller.send('deleteGist', gist);
   assert.ok(!gist.get('called'), 'gist.destroyRecord was not called when not confirmed');
-
-  window.confirm = cacheConfirm;
 });
