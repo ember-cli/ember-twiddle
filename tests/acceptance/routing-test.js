ce4bfe4db17ba4bbe392ee2d9c1b3e247e953339
Gaurav Munjal <gmunjal@hedgeserv.com>
2015-07-31 14:24:26 -0400

    More updates

--- tests/acceptance/routing-test.js
+++ tests/acceptance/routing-test.js
@@ -33,7 +33,7 @@ test('Able to do routing in a gist', function(assert) {
     },
     {
       filename: "twiddle.json",
-      content: "{\n  \"version\": \"0.4.0\",\n  \"dependencies\": {\n    \"jquery\": \"https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.js\",\n    \"ember\": \"https://cdnjs.cloudflare.com/ajax/libs/ember.js/1.13.5/ember.js\",\n    \"ember-data\": \"https://cdnjs.cloudflare.com/ajax/libs/ember-data.js/1.13.5/ember-data.js\"\n  }\n}"
+      content: "{\n  \"version\": \"0.4.0\",\n  \"dependencies\": {\n    \"jquery\": \"https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.js\",\n    \"ember\": \"https://cdnjs.cloudflare.com/ajax/libs/ember.js/1.13.5/ember.js\",\n    \"ember-data\": \"https://cdnjs.cloudflare.com/ajax/libs/ember-data.js/1.13.7/ember-data.js\"\n  }\n}"
     }
   ];
 
