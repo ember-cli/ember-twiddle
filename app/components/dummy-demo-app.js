44648400593722e1f943c99ac2a996a6b61ca1c3
Joost de Vries <joost@closealert.com>
2015-07-31 08:53:08 +0200

    Friendly message when srcdoc is not supported

--- app/components/dummy-demo-app.js
+++ app/components/dummy-demo-app.js
@@ -13,15 +13,23 @@ export default Ember.Component.extend(ResizeMixin, {
       this.element.removeChild(this.element.firstElementChild);
     }
 
-    var ifrm = document.createElement('iframe');
+    let ifrm = document.createElement('iframe');
     ifrm.id = this.iframeId;
+    let supportsSrcDoc = ('srcdoc' in ifrm);
 
-    if (!Ember.testing) {
+    if (!Ember.testing && supportsSrcDoc) {
       ifrm.sandbox = 'allow-scripts allow-forms';
       ifrm.srcdoc = this.get('html');
     }
 
     this.element.appendChild(ifrm);
+
+    if(!supportsSrcDoc && !Ember.testing) {
+      ifrm = (ifrm.contentWindow) ? ifrm.contentWindow : (ifrm.contentDocument.document) ? ifrm.contentDocument.document : ifrm.contentDocument;
+      ifrm.document.open();
+      ifrm.document.write('<p>Your browser doesn\'t support the <code>srcdoc</code> attribute for iframes. Ember Twiddle needs this to run safely.</p><p>Please use the latest version of Chrome, Safari or Firefox.</p><p>More information: <a href="https://github.com/ember-cli/ember-twiddle#browser-support">https://github.com/ember-cli/ember-twiddle#browser-support</a>');
+      ifrm.document.close();
+    }
 
     if (Ember.testing) {
       ifrm = ifrm.contentWindow;
