04a2e09078041e9f43def245959482b498ac77ae
Gaurav Munjal <Gaurav0@aol.com>
2015-08-04 02:56:24 -0700

    Extract file menu into a component

---
+++ app/components/file-menu.js
@@ -0,0 +1,33 @@
+import Ember from "ember";
+
+export default Ember.Component.extend({
+  tagName: 'ul',
+  classNames: ['nav', 'nav-pills', 'file-menu'],
+
+  actions: {
+    addFile(type) {
+      this.sendAction('addFile', type);
+    },
+    renameFile(file) {
+      this.sendAction('renameFile', file);
+    },
+    removeFile(file) {
+      this.sendAction('removeFile', file);
+    },
+    saveGist(model) {
+      this.sendAction('saveGist', model);
+    },
+    share() {
+      prompt('Ctrl + C ;-)', window.location.href);
+    },
+    fork(model) {
+      this.sendAction('fork', model);
+    },
+    deleteGist(model) {
+      this.sendAction('deleteGist', model);
+    },
+    signInViaGithub() {
+      this.sendAction('signInViaGithub');
+    }
+  }
+});
