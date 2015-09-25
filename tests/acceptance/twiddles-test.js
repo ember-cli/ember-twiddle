ff86f7eccf3d26578cb6139a431e9ef9d1b90644
Gaurav Munjal <gmunjal@hedgeserv.com>
2015-09-25 13:29:36 -0400

    Add some more twiddles tests

--- tests/acceptance/twiddles-test.js
+++ tests/acceptance/twiddles-test.js
@@ -8,6 +8,22 @@ module('Acceptance | twiddles', {
     this.application = startApp();
 
     server.create('user', 'octocat');
+    const owner = server.create('owner', {login: 'octocat'});
+    const file = server.create('gist-file', {
+      login: "octocat",
+      filename: "twiddle.json",
+      content: ''
+    });
+    server.create('gist', {
+      id: '35de43cb81fc35ddffb2',
+      owner: owner,
+      files: [file]
+    });
+    server.create('gist', {
+      id: '74bae9a34142370ff5a3',
+      owner: owner,
+      files: [file]
+    });
 
     stubValidSession(this.application, {
       "github-oauth2": {}
@@ -20,10 +36,15 @@ module('Acceptance | twiddles', {
 });
 
 test('visiting /twiddles', function(assert) {
+  assert.expect(4);
+
   visit('/twiddles');
 
   andThen(function() {
     assert.equal($('h1.test-twiddles-header').text().trim(), "My Saved Twiddles");
-  });
 
+    assert.equal($('.saved-twiddles tr').length, 2);
+    assert.equal($('.saved-twiddles tr').eq(0).find('.test-gist-id').text().trim(), '35de43cb81fc35ddffb2');
+    assert.equal($('.saved-twiddles tr').eq(1).find('.test-gist-id').text().trim(), '74bae9a34142370ff5a3');
+  });
 });
