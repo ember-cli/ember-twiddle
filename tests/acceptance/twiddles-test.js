55e5f895f161b1b23efc4a513b7abc66baaf5f64
pangratz <cmueller.418@gmail.com>
2015-10-23 14:31:25 +0200

    Render toolbar in /twiddles template
    This makes the look of /twiddles consistent with the other parts of
    Ember Twiddle. The rendered file menu only contains the "New Twiddle"
    menu item, which creates a new twiddle.

--- tests/acceptance/twiddles-test.js
+++ tests/acceptance/twiddles-test.js
@@ -41,7 +41,7 @@ test('visiting /twiddles', function(assert) {
   visit('/twiddles');
 
   andThen(function() {
-    assert.equal($('h1.test-twiddles-header').text().trim(), "My Saved Twiddles");
+    assert.equal($('.saved-twiddles-header').text().trim(), "My Saved Twiddles");
 
     assert.equal($('.saved-twiddles tr').length, 2);
     assert.equal($('.saved-twiddles tr').eq(0).find('.test-gist-id').text().trim(), '35de43cb81fc35ddffb2');
