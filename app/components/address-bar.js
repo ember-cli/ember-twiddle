ed632bcb1fa066459f817581d572ef3e660b46ae
Gaurav Munjal <gmunjal@hedgeserv.com>
2015-10-28 13:12:46 -0400

    Revert to using enter action instead of change event

--- app/components/address-bar.js
+++
@@ -1,9 +0,0 @@
-import Ember from "ember";
-
-export default Ember.Component.extend({
-  classNames: ["url-bar"],
-
-  change(event) {
-    this.sendAction("urlChanged", event.target.value);
-  }
-});
