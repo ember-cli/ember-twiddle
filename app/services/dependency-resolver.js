6979b3d30f94e9175c0754b9f7bcf0c93a8df2a7
Piotr Pałek <piotrpalek@gmail.com>
2016-01-24 23:12:12 +0100

    bump ember and ember-data versions in dependency resolver

--- app/services/dependency-resolver.js
+++ app/services/dependency-resolver.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
-const EMBER_VERSIONS = ['2.2.0', '2.1.1', '2.0.2', '1.13.11', '1.12.1'];
-const EMBER_DATA_VERSIONS = ['2.3.0', '2.2.1', '2.1.0', '2.0.1', '1.13.15'];
+const EMBER_VERSIONS = ['2.3.0', '2.2.0', '2.1.1', '2.0.2', '1.13.11', '1.12.1'];
+const EMBER_DATA_VERSIONS = ['2.3.3', '2.2.1', '2.1.0', '2.0.1', '1.13.15'];
 
 const VERSION_REGEX = /^\d+.\d+.\d+$/;
 
