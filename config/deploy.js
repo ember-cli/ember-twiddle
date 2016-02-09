73af70a3d8902486ce43a938ca4d375671a3560f
Gaurav Munjal <gmunjal@hedgeserv.com>
2016-02-09 12:31:17 -0500

    Upgrade ember-cli-deploy to 0.5.1

--- config/deploy.js
+++ config/deploy.js
@@ -1,37 +1,44 @@
 /* jshint node: true */
 
-module.exports = {
-  staging: {
-    buildEnv: "staging",
-    store: {
-      type: "S3",
-      accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
-      secretAccessKey: process.env['AWS_ACCESS_KEY_SECRET'],
-      bucket: "canary.ember-twiddle.com",
-      region: "us-west-2"
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {
+      environment: deployTarget
     },
-    assets: {
-      type: "s3",
+    "revision-data": {
+      type: "git-tag-commit"
+    }
+  };
+
+  if (deployTarget === 'staging') {
+    ENV['s3'] = {
       accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
       secretAccessKey: process.env['AWS_ACCESS_KEY_SECRET'],
       bucket: "canary-assets.ember-twiddle.com",
       region: "us-west-2"
-    }
-  },
-  production: {
-    store: {
-      type: "S3",
+    };
+    ENV['s3-index'] = {
       accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
       secretAccessKey: process.env['AWS_ACCESS_KEY_SECRET'],
-      bucket: "ember-twiddle.com",
+      bucket: "canary.ember-twiddle.com",
       region: "us-west-2"
-    },
-    assets: {
-      type: "s3",
+    }
+  }
+
+  if (deployTarget === 'production') {
+    ENV['s3'] = {
       accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
       secretAccessKey: process.env['AWS_ACCESS_KEY_SECRET'],
       bucket: "assets.ember-twiddle.com",
       region: "us-west-2"
+    };
+    ENV['s3-index'] = {
+      accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
+      secretAccessKey: process.env['AWS_ACCESS_KEY_SECRET'],
+      bucket: "ember-twiddle.com",
+      region: "us-west-2"
     }
   }
+
+  return ENV;
 };
