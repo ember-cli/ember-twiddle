b9542a4affa74f4947bd2034cccfe47098e5f282
Gaurav Munjal <gmunjal@hedgeserv.com>
2016-02-12 06:40:28 -0500

    Add verbose flag

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -10,9 +10,9 @@ fi
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   # Deploy to staging on a merge to master
-  ember deploy staging --activate
+  ember deploy staging --verbose --activate
 
 elif [ -n "$TRAVIS_TAG" ]; then
   # Deploy to production on a tag
-  ember deploy production --activate
+  ember deploy production --verbose --activate
 fi
