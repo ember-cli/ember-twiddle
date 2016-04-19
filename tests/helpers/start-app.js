c358c9a9182bfc3164851284921b92179bb96900
Gaurav Munjal <Gaurav0@aol.com>
2016-04-18 22:16:04 -0700

    Update notify

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -2,13 +2,14 @@ import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
 
-const { assign, merge, run } = Ember;
+const { run } = Ember;
+const assign = Ember.assign || Ember.merge;
 
 export default function startApp(attrs) {
   let application;
 
-  let attributes = (assign || merge)({}, config.APP);
-  attributes = (assign || merge)(attributes, attrs); // use defaults, but you can override;
+  let attributes = assign({}, config.APP);
+  attributes = assign(attributes, attrs); // use defaults, but you can override;
 
   run(() => {
     application = Application.create(attributes);
