dd4790463b20844e2055eed2a0826509ff699d29
Gaurav Munjal <gmunjal@hedgeserv.com>
2016-05-02 11:18:30 -0400

    Add tests for saving a gist

--- mirage/config.js
+++ mirage/config.js
@@ -1,5 +1,6 @@
 import AddonFixture from "./fixtures/addon";
 import config from "../config/environment";
+import Mirage, { faker } from "ember-cli-mirage";
 
 export default function() {}
 
@@ -20,6 +21,18 @@ export function testConfig() {
   this.get('/gists/:id/:rev_id', function(schema, request) {
     let id = request.params.id;
     return schema.db["gistRevisions"].find(id);
+  });
+
+  this.post('/gists', function(schema, request) {
+    let gist = server.create('gist', { id: faker.random.uuid() });
+    let response = Ember.assign(gist, JSON.parse(request.requestBody));
+    return new Mirage.Response(200, {}, response);
+  });
+
+  this.patch('/gists/:id', (schema, request) => {
+    let response = JSON.parse(request.requestBody);
+    response.id = request.params.id;
+    return new Mirage.Response(200, {}, response);
   });
 
   this.get('/user', function(schema) {
---
+++ tests/acceptance/save-test.js
@@ -0,0 +1,77 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'ember-twiddle/tests/helpers/module-for-acceptance';
+import { stubValidSession } from 'ember-twiddle/tests/helpers/torii';
+
+moduleForAcceptance('Acceptance | save gist', {
+  beforeEach: function() {
+    this.cacheConfirm = window.confirm;
+    window.confirm = () => true;
+
+    server.create('user', 'octocat');
+  },
+
+  afterEach: function() {
+    window.confirm = this.cacheConfirm;
+  }
+});
+
+test('can save a gist with an id', function(assert) {
+  // set owner of gist as currently logged in user
+  stubValidSession(this.application, {
+    currentUser: {login: "Gaurav0"},
+    "github-oauth2": {}
+  });
+
+  runGist([
+    {
+      filename: 'application.template.hbs',
+      content: 'hello world!'
+    }
+  ]);
+
+  visit('/35de43cb81fc35ddffb2');
+
+  andThen(function () {
+    assert.equal(find('.test-unsaved-indicator').length, 0, "No unsaved indicator shown");
+  });
+
+  fillIn('.title input', "my twiddle");
+
+  andThen(function() {
+    assert.equal(find('.title input').val(), "my twiddle");
+    assert.equal(find('.test-unsaved-indicator').length, 1, "Changing title triggers unsaved indicator");
+  });
+
+  click('.test-save-action');
+
+  andThen(function() {
+    assert.equal(find('.test-unsaved-indicator').length, 0, "No unsaved indicator shown");
+  });
+});
+
+test('can save a gist without an id', function(assert) {
+  // set owner of gist as currently logged in user
+  stubValidSession(this.application, {
+    currentUser: {login: "Gaurav0"},
+    "github-oauth2": {}
+  });
+
+  visit('/');
+
+  andThen(function () {
+    assert.equal(find('.test-unsaved-indicator').length, 0, "No unsaved indicator shown");
+  });
+
+  fillIn('.title input', "my twiddle");
+
+  andThen(function() {
+    assert.equal(find('.title input').val(), "my twiddle");
+    assert.equal(find('.test-unsaved-indicator').length, 1, "Changing title triggers unsaved indicator");
+  });
+
+  click('.test-save-action');
+
+  andThen(function() {
+    assert.equal(find('.test-unsaved-indicator').length, 0, "No unsaved indicator shown");
+  });
+});
