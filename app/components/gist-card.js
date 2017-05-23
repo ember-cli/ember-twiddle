928ae1d6823f8c5e62c9bfc8434254ecfa987af2
Ilya Radchenko <ilya@burstcreations.com>
2017-05-23 19:13:23 -0400

    gist card menus

--- app/components/gist-card.js
+++ app/components/gist-card.js
@@ -9,7 +9,30 @@ export default Ember.Component.extend({
   // show fork option only if does not belong to user and is not a revision, otherwise show copy
   // Github api does not permit forking if you own the gist already
   // Github does not provide api for forking a revision
-  showFork: computed('gist.ownerLogin', 'session.currentUser.login', 'isRevision', function() {
-    return !this.get('isRevision') && this.get('gist.ownerLogin') !== this.get('session.currentUser.login');
+  showFork: computed('gist.ownerLogin', 'session.currentUser.login', 'isRevision', 'gist.isNew', function() {
+    let isNew = this.get('gist.isNew');
+    let notRevision = !this.get('isRevision');
+    let notOwner = this.get('gist.ownerLogin') !== this.get('session.currentUser.login');
+    
+    return !isNew && notRevision && notOwner;
   }),
+
+  actions: {
+    share() {
+      prompt('Ctrl + C ;-)', window.location.href);
+    },
+
+    embed() {
+      let src = window.location.href.split('?')[0];
+      src += '?fullScreen=true';
+      let responsive = document.createElement('div');
+      responsive.style.cssText = 'position:relative;height:0;overflow:hidden;max-width:100%;padding-bottom:56.25%;'; // 16:9
+      let iframe = document.createElement('iframe');
+      iframe.src = src;
+      iframe.style.cssText = 'position:absolute;top:0;left:0;width:100%;height:100%;';
+      responsive.appendChild(iframe);
+      let embedCode = responsive.outerHTML;
+      prompt('Ctrl + C ;-)', embedCode);
+    }
+  }
 });
