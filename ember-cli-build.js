bd7d5a21b4f580a44ad67b15330e9e3f47aa97c5
Gaurav Munjal <gaurav@peeriq.com>
2018-06-19 11:38:18 -0400

    Fix tests for tests

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -150,8 +150,6 @@ module.exports = function(defaults) {
   });
 
   let transpiledInitializersTree = babelTranspiler(baseInitializersTree, babelOpts());
-
-  let finalTestHelpersTree = buildAddonTree('ember-test-helpers');
   
   let preprocessJs = app.registry.registry.js[0].toTree;
   let buildPreprocessedAddon = function(addonName) {
@@ -197,7 +195,7 @@ module.exports = function(defaults) {
     outputFile: '/assets/twiddle-deps.js'
   });
 
-  return app.toTree(mergeTrees([twiddleVendorTree, loaderTree, testLoaderTree, finalTestHelpersTree, finalQUnitTree]));
+  return app.toTree(mergeTrees([twiddleVendorTree, loaderTree, testLoaderTree, finalQUnitTree]));
 };
 
 function buildAddonTree(addonName) {
