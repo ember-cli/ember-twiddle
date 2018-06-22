06c081a6cdeacff944595242b8e63c7a0aca829c
gokatz <gokulkathirvel@live.com>
2018-06-23 01:30:21 +0530

    fix test server shutdown

--- tests/helpers/destroy-app.js
+++ tests/helpers/destroy-app.js
@@ -3,6 +3,6 @@ import { run } from '@ember/runloop';
 export default function destroyApp(application) {
   run(() => {
     application.destroy();
-    window.server.shutdown();
+    window.server && window.server.shutdown();
   });
 }
