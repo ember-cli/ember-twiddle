c7e1ddb9790a46e711d809d5d2e38978c58fc4ba
Ilya Radchenko <knownasilya@gmail.com>
2020-02-18 16:14:52 -0500

    currentURL

--- tests/acceptance/columns-test.js
+++ tests/acceptance/columns-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit } from '@ember/test-helpers';
+import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
