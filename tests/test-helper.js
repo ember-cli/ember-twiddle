85759441996e72f0671738ed31056c3b11d1b141
Gaurav Munjal <Gaurav0@aol.com>
2020-02-19 12:40:38 -0500

    fix location of test app

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -4,7 +4,6 @@ import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
 let attributes = Object.assign({}, config.APP);
-attributes.rootElement = "#main-test-app";
 setApplication(Application.create(attributes));
 
 start();
