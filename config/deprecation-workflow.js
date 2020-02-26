b6f3b4205961c5d7819351cc0aaed2b5e62336e1
Gaurav Munjal <Gaurav0@aol.com>
2020-02-26 01:57:38 -0500

    update flush deprecations

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,6 +1,8 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-
+    { handler: "silence", matchId: "ember-runtime.deprecate-copy-copyable" },
+    { handler: "silence", matchId: "ember-data:method-calls-on-destroyed-store" },
+    { handler: "silence", matchId: "ember-component.send-action" }
   ]
 };
