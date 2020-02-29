066ec7a3a09e7fba6d9c4c331582e379c7456b50
Ilya Radchenko <knownasilya@gmail.com>
2020-02-29 17:14:22 -0500

    mobile

--- app/components/gist-header.js
+++ app/components/gist-header.js
@@ -3,5 +3,6 @@ import Component from '@ember/component';
 export default Component.extend({
   classNames: ['gist-header'],
   classNameBindings: ['open'],
-  tagName: 'nav'
+  tagName: 'nav',
+  open: true
 });
