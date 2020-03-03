2987b965556bf96056822bcde46dfb486adfb74e
Gaurav Munjal <Gaurav0@aol.com>
2020-03-03 11:18:05 -0500

    upgrade ember and ember data to 3.12

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -5,6 +5,7 @@ self.deprecationWorkflow.config = {
     { handler: "silence", matchId: "ember-runtime.deprecate-copy-copyable" },
     { handler: "silence", matchId: "ember-polyfills.deprecate-merge" },
     { handler: "silence", matchId: "ember-data:method-calls-on-destroyed-store" },
+    { handler: "silence", matchId: "ember-data:evented-api-usage" },
     { handler: "silence", matchId: "ember-component.send-action" },
     { handler: "silence", matchId: "ember-views.curly-components.jquery-element" },
     { handler: "silence", matchId: "ember-test-helpers.rendering-context.jquery-element" }
