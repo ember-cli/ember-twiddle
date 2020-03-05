204d5eadae0fc9e9e17a9c34297930b3ce1fa83f
Gaurav Munjal <Gaurav0@aol.com>
2020-03-05 10:28:07 -0500

    flush deprecations

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,6 +1,7 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
+    { handler: "silence", matchId: "ember.globals-resolver" },
     { handler: "silence", matchId: "deprecate-router-events" },
     { handler: "silence", matchId: "ember-runtime.deprecate-copy-copyable" },
     { handler: "silence", matchId: "ember-polyfills.deprecate-merge" },
@@ -8,6 +9,7 @@ self.deprecationWorkflow.config = {
     { handler: "silence", matchId: "ember-data:evented-api-usage" },
     { handler: "silence", matchId: "ember-component.send-action" },
     { handler: "silence", matchId: "ember-views.curly-components.jquery-element" },
-    { handler: "silence", matchId: "ember-test-helpers.rendering-context.jquery-element" }
+    { handler: "silence", matchId: "ember-test-helpers.rendering-context.jquery-element" },
+    { handler: "silence", matchId: "globals-resolver" }
   ]
 };
