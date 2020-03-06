e939b4cedb5d3f3a6587a43dc063ee73e1779006
Ilya Radchenko <knownasilya@gmail.com>
2020-03-06 14:32:00 -0500

    sidebar-layout test

--- tests/integration/components/sidebar-layout-test.js
+++ tests/integration/components/sidebar-layout-test.js
@@ -1,26 +1,46 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { helper } from '@ember/component/helper';
+
 
 module('Integration | Component | sidebar-layout', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function() {
+    this.owner.register('helper:route-action', helper(function noop() {
+      return () => {};
+    }));
+    this.owner.register('helper:transition-to', helper(function noop() {
+      return () => {};
+    }));
+    this.owner.register('template:components/test-sidebar', hbs`test sidebar`);
+  });
 
-    await render(hbs`<SidebarLayout />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
+  test('it toggles', async function(assert) {
+    this.set('isSidebarOpen', true);
     await render(hbs`
-      <SidebarLayout>
-        template block text
+      <SidebarLayout
+        @sidebarComponent={{component "test-sidebar"}}
+        @isSidebarOpen={{this.isSidebarOpen}}
+        @onSidebarUpdated={{fn (mut this.isSidebarOpen)}}
+      as |sidebar|>
+        {{#if sidebar.isSidebarOpen}}
+          <button class="close" {{on "click" sidebar.hideSidebar}}>Close</button>
+        {{else}}
+          <button class="open" {{on "click" sidebar.showSidebar}}>Open</button>
+        {{/if}}
+        Content Here
       </SidebarLayout>
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('.sidebar').hasText('Close Menu test sidebar Sign in Help GitHub Issue tracker');
+    assert.dom('.content').hasText('Close Content Here');
+    await click('.close');
+    assert.dom('.sidebar').doesNotExist();
+    assert.dom('.content').hasText('Open Content Here');
+    await click('.open');
+    assert.dom('.content').hasText('Close Content Here');
   });
 });
