0f6595c3901e8438ec5490d4b08d1a4b84cfbdf8
Gaurav Munjal <Gaurav0@aol.com>
2020-08-08 13:32:35 -0400

    fix broken login

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -2,11 +2,13 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   urlForFindRecord(id, typeKey) {
-    if(id==='current') {
-      var url = this._buildURL(typeKey);
-      return url.substring(0, url.length-1);
-    }
+    // Github no longer lets you query for another user by id
+    // requires you to use user's login name
+    // but I never was actually doing so
+    // so this code now assumes that always querying for current user
+    // Gaurav - 8/8/20
 
-    return this._buildURL(typeKey, id);
+    let url = this._buildURL(typeKey);
+    return url.substring(0, url.length-1);
   }
 });
